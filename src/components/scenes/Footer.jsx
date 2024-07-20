56c1b6b5e8b1482b4fd70aab9e26c7e96f886274
DanielGad <adeyemidanielgad@gmail.com>
2024-07-20 04:06:30 +0100

    Update Changes

--- src/components/scenes/Footer.jsx
+++ src/components/scenes/Footer.jsx
@@ -3,7 +3,7 @@ import '../../assets/footer.css'
 const Footer = () => {
   return (
     <div className='footer-cont'>
-      
+        <div>© Gadhub All Rights Reserved 2024.</div>
     </div>
   )
 }
