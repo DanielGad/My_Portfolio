dea80ace930c7643fb6e30f20caadcb2bba6bdfb
DanielGad <adeyemidanielgad@gmail.com>
2024-09-01 23:53:37 +0100

    Change Theme

--- src/components/scenes/Head.jsx
+++ src/components/scenes/Head.jsx
@@ -1,6 +1,7 @@
 import '../../assets/head.css'
 import {  useContext } from 'react';
 import { Context } from '../Context';
+import { Link } from 'react-scroll';
 
 const Head = () => {
   
@@ -14,11 +15,21 @@ const Head = () => {
         <div className="name">Daniel Gad
         </div>
         <div className='work-skill'>A Frontend Developer and Graphics Designer</div>
+
+        <div className="button-2">
+        <a href="./Daniel_Gad_CV.pdf" download="Daniel_Gad_CV"><button className="cv">Download My CV</button></a>
+          <Link to={'contact'} smooth={true} duration={500} offset={-150}>
+          <button className="contactme">Contact Me</button>
+          </Link>
+        </div>
       </div>
       <div className={` gad-image-cont right-slide-in-element ${isVisible ? 'visible' : ''}`}>
+        
         <img src="./gad.png" alt="gad" className='gad-image' width={'120%'} />
       </div>
-      
+      {/* <div className='star-1'>
+          <img src="./star.png" alt="200px" />
+        </div> */}
     </div>
   )
 }
