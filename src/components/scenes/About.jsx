6e46d04747d2c95cfc1ade1566618ec3ba13421d
DanielGad <adeyemidanielgad@gmail.com>
2024-09-02 08:25:19 +0100

    Change Theme

--- src/components/scenes/About.jsx
+++ src/components/scenes/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className='about-body' id='about'>
       <div className='abt-tag slide-in right-slide-in'>AboutMe</div>
       <div className='text-cont'>
-        <p className={` slide-in txt-1 bottom-slide-in`}>Hello there! I&apos;m Adeyemi Oluwagbenga Daniel, a passionate front-end developer and creative graphic designer with a love for crafting digital experiences that blend aesthetics and functionality. With two years of experience, I&apos;ve honed my skills and expertise to bring visions to life on the web.</p>
+        <p className={` slide-in txt-1 bottom-slide-in`}>Hello there! I&apos;m Adeyemi Oluwagbenga Daniel, a passionate front-end developer and creative graphic designer with a love for crafting digital experiences that blend aesthetics and functionality. With dedication, I&apos;ve honed my skills and expertise to bring visions to life on the web.</p>
         <h3>Designing Experiences, One Pixel at a Time</h3>
         <p className='txt-1 slide-in bottom-slide-in'>From a young age, I&apos;ve been captivated by the world of design and technology. This fascination led me to pursue a career that seamlessly merges these two passions. As a graphic designer, I have an eye for detail and a knack for translating ideas into visually appealing designs. Each project is a canvas where I blend colors, typography, and layout to create a visual language that resonates with users.</p>
         <h3>Bringing Designs to Life through Front-End Development</h3>
