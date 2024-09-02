1a3b955bef9576679e58d31b4c2df91a590c0a27
DanielGad <adeyemidanielgad@gmail.com>
2024-09-02 01:19:56 +0100

    Change Theme

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,27 @@
 import '../assets/menu.css';
 import GadLogo from '/G.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Context } from './Context';
 import { Link } from 'react-scroll';
 
 const Menu = () => {
   const { isMenuOpen, isSmallScreen, toggleMenu, isScrolled  } = useContext(Context);
+
+  const storedTheme = localStorage.getItem('theme') || 'light';
+  const [theme, setTheme] = useState(storedTheme);
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
 
   return (
     <div className={`menu-container ${isScrolled ? 'scrolled' : ''}`}>
@@ -18,7 +32,10 @@ const Menu = () => {
         
       </div>
 
-      <div>
+      <div className='ccc'>
+      <button onClick={toggleTheme} className='theme' style={{ fontSize: '24px', background: 'none', border: 'none', cursor: 'pointer' }}> 
+        {theme === 'light' ? <FontAwesomeIcon icon={faMoon} /> : <FontAwesomeIcon icon={faSun} />}
+      </button>
       {isSmallScreen ? 
         <div className={`icon-div`} onClick={toggleMenu}>
           <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars }/>
